0928f33374e5fe71d1dbf1d5c4bbcec1eb8b8aa6
moonypoo <47288881+moonypoo@users.noreply.github.com>
2019-02-25 13:00:20 -0800

    Update square-circles.js

--- square-circles.js
+++ square-circles.js
@@ -1,5 +1,6 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER)
 }
 
 var x = []; // new empty array
@@ -13,9 +14,11 @@ function draw() {
   y.push(mouseY); // equivalent to append(y, mouseY)
 
   for (var i = 0; i < x.length; i = i + 10) {
-    square(x[i], y[i], 1 + (x.length - i));
+    var w = 1 + (x.length - i);
+    //rect(x[i] - w/2, y[i] - w/2, w, w);
+    rect(x[i], y[i], w, w)
   }
 
-  x = x.slice(-50); // keep the last 50 x values
-  y = y.slice(-50); // keep the last 50 y values
+  x = x.slice(-150); // keep the last 50 x values
+  y = y.slice(-150); // keep the last 50 y values
 }
